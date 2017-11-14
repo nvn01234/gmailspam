a6ba87d4420f26557087858cbd514207edf4dd3c
huyenuet <thanhhuyenk59uet@gmail.com>
2017-11-14 15:01:08 +0700

    commit

--- static/js/main.js
+++ static/js/main.js
@@ -1,37 +1,11 @@
-/**
- * Created by smart on 10/11/2017.
- */
-
-// function submit() {
-//     var input_email = $("#input-email").val();
-//     $.post(
-//         "",         //name of request
-//         {input: input_email},
-//         function (response) {
-//             $("#result").html(response);
-//         }
-//     );
-//     $("#result").html("spam");
-// }
-// $.ajax({
-//   type: "POST",
-//   url: url,  // url server của thắng
-//   data: data,  // data input
-//   success: success,  //  hàm callback khi success
-//   dataType: dataType  // type data trả về
-// });
 
 $("#submit").click(function (event) {
-    // Stop form from submitting normally
-    // event.preventDefault();
 
     // Get some values from elements on the page:
     var $form = $("#submit-form"),
         term = $form.find("input[id='input-email']").val(),
         url = $form.attr("action");
 
-    //send data using post
-    // var posting = $.post(url, {input: term});
     console.log($form.serialize());
     $.ajax({
         url: '/result',
@@ -45,10 +19,4 @@ $("#submit").click(function (event) {
             console.log(e);
         }
     });
-
-    //Put result in a div
-    // posting.done(function (data) {
-    //    var content = $(data).find("#content");
-    //     $("#result").empty().append(content);
-    // });
 });
